cd3de7661c877e7884e0e9c637aee306a8304cb2
hiromasae <hiro@fleegix.org>
2025-04-14 17:55:23 -0700

    Initial Commit

---
+++ multgame/multgameApp.swift
@@ -0,0 +1,17 @@
+//
+//  multgameApp.swift
+//  multgame
+//
+//  Created by Hiro Eernisse on 4/14/25.
+//
+
+import SwiftUI
+
+@main
+struct multgameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
